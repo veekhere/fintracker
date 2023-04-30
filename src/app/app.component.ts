89f42741a235db79074de01d7f5215f2285e1bd7
Vee <VKuguenko@croc.ru>
2023-04-30 18:55:37 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fin-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'fintracker';
+}
